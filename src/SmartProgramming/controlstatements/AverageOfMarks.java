a1c3bfff687142b18e5971af19c110a9bcb4d4c1
kumarshyam7777 <kumar.shyam7777@gmail.com>
2020-10-05 21:52:23 +0530

    "Average Calculation"

---
+++ src/SmartProgramming/controlstatements/AverageOfMarks.java
@@ -0,0 +1,26 @@
+package SmartProgramming.controlstatements;
+
+import java.util.Scanner;
+
+public class AverageOfMarks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the marks1: ");
+        int marks_1 = scanner.nextInt();
+        System.out.print("Enter the marks2: ");
+        int marks_2 = scanner.nextInt();
+        System.out.print("Enter the marks3: ");
+        int marks_3 = scanner.nextInt();
+        System.out.print("Enter the marks4: ");
+        int marks_4 = scanner.nextInt();
+        System.out.print("Enter the marks5: ");
+        int marks_5 = scanner.nextInt();
+        float result = (marks_1+marks_2+marks_3+marks_4+marks_5);
+        System.out.println(result);
+        float avg = result/5;
+        System.out.println("The 5 subjects marks of average are: " + avg );
+
+
+    }
+}
+
