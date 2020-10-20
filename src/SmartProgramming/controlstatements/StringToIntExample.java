bc113a6f6a2f38b1adfea7465fed551e6f08dd92
kumarshyam7777 <kumar.shyam7777@gmail.com>
2020-10-20 20:52:05 +0530

    String to Integer conversion example

---
+++ src/SmartProgramming/controlstatements/StringToIntExample.java
@@ -0,0 +1,14 @@
+package SmartProgramming.controlstatements;
+
+public class StringToIntExample {
+    public static void main(String[] args) {
+        String str = "12345";
+        int num1 = 55;
+        int num2 = Integer.parseInt(str);
+        int sum = num1 + num2;
+        System.out.println("The sum of 2 nos: " + sum);
+        System.out.println(Integer.toString(num1));
+
+    }
+
+}
