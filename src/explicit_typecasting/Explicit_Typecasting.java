30362ebb74b6f5eca05c89a473ca08dffbce2fb5
kumarshyam7777 <kumar.shyam7777@gmail.com>
2021-06-22 20:22:50 +0530

    New Commit

---
+++ src/explicit_typecasting/Explicit_Typecasting.java
@@ -0,0 +1,11 @@
+package explicit_typecasting;
+
+public class Explicit_Typecasting {
+    public static void main(String[] args) {
+        byte b = (byte) 134;
+        System.out.println("The typecasting b value:" + b );
+        char ch = 'T';
+        int x = ch;
+    }
+
+}
